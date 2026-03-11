800d712bdfc10de40855cf31b7adf03933755337
228141 <jailton.sp@hotmail.com>
2026-03-11 14:30:33 -0300

    Create script Notificação de multa

---
+++ Notificação_Multa/script_preencherVeiculo.js
@@ -0,0 +1,201 @@
+var placa = document.getElementById("placa").value;
+
+if (placa == "1") {
+    document.getElementById("veiculo").value = "Virtus";
+	document.getElementById("renavam").value = "01382188509";
+
+    model.veiculo = "Virtus";
+    model.renavam = "01382188509";
+}
+
+if (placa == "2") {
+    document.getElementById("veiculo").value = "Virtus";
+	document.getElementById("renavam").value = "01382190333";
+
+    model.veiculo = "Virtus";
+    model.renavam = "01382190333";
+}
+
+if (placa == "3") {
+    document.getElementById("veiculo").value = "Virtus";
+	document.getElementById("renavam").value = "01382190260";
+
+    model.veiculo = "Virtus";
+    model.renavam = "01382190260";
+}
+
+if (placa == "4") {
+    document.getElementById("veiculo").value = "Virtus";
+	document.getElementById("renavam").value = "01382190228";
+
+    model.veiculo = "Virtus";
+    model.renavam = "01382190228";
+}
+
+if (placa == "5") {
+    document.getElementById("veiculo").value = "Virtus";
+	document.getElementById("renavam").value = "01382190104";
+
+    model.veiculo = "Virtus";
+    model.renavam = "01382190104";
+}
+
+if (placa == "6") {
+    document.getElementById("veiculo").value = "Virtus";
+	document.getElementById("renavam").value = "01382188649";
+
+    model.veiculo = "Virtus";
+    model.renavam = "01382188649";
+}
+
+if (placa == "7") {
+    document.getElementById("veiculo").value = "Virtus";
+	document.getElementById("renavam").value = "01382189629";
+
+    model.veiculo = "Virtus";
+    model.renavam = "01382189629";
+}
+
+if (placa == "8") {
+    document.getElementById("veiculo").value = "Virtus";
+	document.getElementById("renavam").value = "01382189564";
+
+    model.veiculo = "Virtus";
+    model.renavam = "01382189564";
+}
+
+if (placa == "9") {
+    document.getElementById("veiculo").value = "Virtus";
+	document.getElementById("renavam").value = "01382191496";
+
+    model.veiculo = "Virtus";
+    model.renavam = "01382191496";
+}
+
+if (placa == "10") {
+    document.getElementById("veiculo").value = "Virtus";
+	document.getElementById("renavam").value = "01382190686";
+
+    model.veiculo = "Virtus";
+    model.renavam = "01382190686";
+}
+
+if (placa == "11") {
+    document.getElementById("veiculo").value = "Virtus";
+	document.getElementById("renavam").value = "01382190775";
+
+    model.veiculo = "Virtus";
+    model.renavam = "01382190775";
+}
+
+if (placa == "12") {
+    document.getElementById("veiculo").value = "Virtus";
+	document.getElementById("renavam").value = "01382190872";
+
+    model.veiculo = "Virtus";
+    model.renavam = "01382190872";
+}
+
+if (placa == "13") {
+    document.getElementById("veiculo").value = "Virtus";
+	document.getElementById("renavam").value = "01382189661";
+
+    model.veiculo = "Virtus";
+    model.renavam = "01382189661";
+}
+
+if (placa == "14") {
+    document.getElementById("veiculo").value = "Virtus";
+	document.getElementById("renavam").value = "01382189793";
+
+    model.veiculo = "Virtus";
+    model.renavam = "01382189793";
+}
+
+if (placa == "15") {
+    document.getElementById("veiculo").value = "Virtus";
+	document.getElementById("renavam").value = "01382189084";
+
+    model.veiculo = "Virtus";
+    model.renavam = "01382189084";
+}
+
+if (placa == "16") {
+    document.getElementById("veiculo").value = "Virtus";
+	document.getElementById("renavam").value = "01382188673";
+
+    model.veiculo = "Virtus";
+    model.renavam = "01382188673";
+}
+
+if (placa == "17") {
+    document.getElementById("veiculo").value = "Virtus";
+	document.getElementById("renavam").value = "01382190198";
+
+    model.veiculo = "Virtus";
+    model.renavam = "01382190198";
+}
+
+if (placa == "18") {
+    document.getElementById("veiculo").value = "Virtus";
+	document.getElementById("renavam").value = "01382190619";
+
+    model.veiculo = "Virtus";
+    model.renavam = "01382190619";
+}
+
+if (placa == "19") {
+    document.getElementById("veiculo").value = "Virtus";
+	document.getElementById("renavam").value = "01382190546";
+
+    model.veiculo = "Virtus";
+    model.renavam = "01382190546";
+}
+
+if (placa == "20") {
+    document.getElementById("veiculo").value = "Virtus";
+	document.getElementById("renavam").value = "01382190465";
+
+    model.veiculo = "Virtus";
+    model.renavam = "01382190465";
+}
+
+if (placa == "21") {
+    document.getElementById("veiculo").value = "Virtus";
+	document.getElementById("renavam").value = "01382190406";
+
+    model.veiculo = "Virtus";
+    model.renavam = "01382190406";
+}
+
+if (placa == "22") {
+    document.getElementById("veiculo").value = "Virtus";
+	document.getElementById("renavam").value = "01382190929";
+
+    model.veiculo = "Virtus";
+    model.renavam = "01382190929";
+}
+
+if (placa == "23") {
+    document.getElementById("veiculo").value = "Virtus";
+	document.getElementById("renavam").value = "01382189122";
+
+    model.veiculo = "Virtus";
+    model.renavam = "01382189122";
+}
+
+if (placa == "24") {
+    document.getElementById("veiculo").value = "Spin";
+	document.getElementById("renavam").value = "01392110359";
+
+    model.veiculo = "Spin";
+    model.renavam = "01392110359";
+}
+
+if (placa == "25") {
+    document.getElementById("veiculo").value = "Saveiro";
+	document.getElementById("renavam").value = "00501596844";
+
+    model.veiculo = "Saveiro";
+    model.renavam = "00501596844";
+}
