0c40720837a0b9f22108b86d77f08af31f2888a3
228141 <jailton.sp@hotmail.com>
2026-03-17 09:50:08 -0300

    Create controle_frota

---
+++ Notificação_Multa/controle_frota.js
@@ -0,0 +1,17 @@
+model.descricao = 
+
+"OFÍCIO DE ORIGEM: " + model.oficioOrigem + "\n" +
+"ATI nº: " + model.ait + "\n" +
+"DATA DA EMISSÃO: "  + model.dataEmissao + "\n" +
+"INFRAÇÃO: "  + model.infracao + "\n" +
+"ENQUADRAMENTO: "  + model.enquadramento + "\n" +
+"LOCAL: "  + model.local + "\n" +
+"DATA DA INFRAÇÃO: "  + model.dataInfracao + "\n" +
+"HORA DA INFRAÇÃO: "  + model.horaInfracao + "\n" +
+"NATUREZA: "  + model.natureza + "\n" +
+"PONTUAÇÃO: "  + model.pontuacao + "\n" +
+"ARTIGO DO CTB: "  + model.artigo + "\n" +
+"DATA LIMITE INDINCAÇÃO CONDUTOR / DEFESA AUTUAÇÃO: "  + model.dataLimite + "\n" +
+"AGENTE DE TRÂNSITO: "  + model.agente + "\n" +
+"NUM. RENAINF: "  + model.renainf + "\n" + 
+    "(Insira demais informações).";
