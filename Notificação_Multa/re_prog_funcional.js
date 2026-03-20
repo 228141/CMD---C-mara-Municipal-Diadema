51e1d183baa0c2f03ef98acae9c64a5ac0bd5932
228141 <jailton.sp@hotmail.com>
2026-03-20 17:07:40 -0300

    Create re_prog_funciona.js

---
+++ Notificação_Multa/re_prog_funcional.js
@@ -0,0 +1,3 @@
+model.descricao = 
+
+"Eu, " + model.nomeInteressado + ", cargo, " + model.cargo + " matrícula nº " + model.matricula + "lotado no " + model.depInteressado + "venho, respeitosamente requerer o deferimento da solicitação da progressão funcional.";
