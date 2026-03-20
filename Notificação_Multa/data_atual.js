e961a5c39da01c9c5b85f24bfc38bcfdc069fd68
228141 <jailton.sp@hotmail.com>
2026-03-20 16:54:56 -0300

    Atualização

---
+++ Notificação_Multa/data_atual.js
@@ -0,0 +1,10 @@
+var data = new Date();
+var meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
+var dia = String(data.getDate()).padStart(2, '0');
+var mes = data.getMonth();
+var ano = data.getFullYear();
+var hours = String(data.getHours()).padStart(2, '0');
+var minutes = String(data.getMinutes()).padStart(2, '0');
+var seconds = String(data.getSeconds());
+dataAtual = "Diadema: " + dia + ' de ' + meses[mes] + ' de ' + ano +  ' - ' +  hours + ':' + minutes ;
+model.data = dataAtual
