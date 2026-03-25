cf9901e7b940119a4854621a94e082f128f8d1b1
228141 <jailton.sp@hotmail.com>
2026-03-25 17:19:27 -0300

    Atualização script - Solicitação de Progressão Funcional

--- Notificação_Multa/re_prog_funcional.js
+++ Notificação_Multa/re_prog_funcional.js
@@ -1,3 +1,15 @@
-model.descricao = 
+var tiposProgressao = {
+    1: `Solicitação de Progressão Funcional\n 
+EXCELENTÍSSIMO SENHOR PRESIDENTE DA CÂMARA MUNICIPAL DE DIADEMA
 
-"Eu, " + model.nomeInteressado + ", cargo, " + model.cargo + " matrícula nº " + model.matricula + "lotado no " + model.depInteressado + "venho, respeitosamente requerer o deferimento da solicitação da progressão funcional.";
+Em atendimento ao disposto nos Artigos 21, 22 e 23 da Resolução nº 003, de 15 de junho de 2022, eu, ${model.nomeInteressado}, no cargo atual de ${model.cargoAtual}, Matrícula nº ${model.matriculaInteressado}, lotado na ${model.nomeSetorInteressado}, venho, respeitosamente, solicitar a concessão de Progressão Funcional Vertical.`,
+
+    2: `Solicitação de Progressão Funcional\n 
+EXCELENTÍSSIMO SENHOR PRESIDENTE DA CÂMARA MUNICIPAL DE DIADEMA
+
+Em atendimento nos termos da Resolução nº 003/2022 e alterações posteriores (Resoluções nº 017/2023 e nº 003/2026).
+
+Em atendimento ao disposto nos Artigos 24 e 34-A da Resolução nº 003, de 15 de junho de 2022, e alterações posteriores, eu, ${model.nomeInteressado}, no cargo atual de ${model.cargoAtual}, Matrícula nº ${model.matriculaInteressado}, lotado na ${model.nomeSetorInteressado}, venho, respeitosamente, solicitar a concessão de Progressão Funcional Horizontal.`
+};
+
+model.descricao = tiposProgressao[Number(model.tipoProgressao)] || "";
