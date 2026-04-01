e1d4b27584ccadb67e903ee5b3dc61cdb70785af
228141 <jailton.sp@hotmail.com>
2026-04-01 10:40:33 -0300

    Update data_atual.js

--- Notificação_Multa/data_atual.js
+++ Notificação_Multa/data_atual.js
@@ -8,3 +8,15 @@ var minutes = String(data.getMinutes()).padStart(2, '0');
 var seconds = String(data.getSeconds());
 dataAtual = "Diadema: " + dia + ' de ' + meses[mes] + ' de ' + ano +  ' - ' +  hours + ':' + minutes ;
 model.data = dataAtual
+
+//-------------------------
+
+// PreencherCampoData
+
+var dataAtual = new Date();
+var dia = dataAtual.getDate();
+var mes = dataAtual.toLocaleString('pt-BR', { month: 'long'});
+var ano = dataAtual.getFullYear();
+
+
+model.data = 'Diadema, ' + dia + ' de '+ mes + ' de ' + ano + '.'
