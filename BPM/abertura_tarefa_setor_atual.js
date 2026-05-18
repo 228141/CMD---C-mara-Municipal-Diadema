4c8134c2fc14d7d4dd7e1e4fe575630a27a96e5f
228141 <jailton.sp@hotmail.com>
2026-05-18 17:59:54 -0300

    Atualização

---
+++ BPM/abertura_tarefa_setor_atual.js
@@ -0,0 +1,10 @@
+setTimeout(() => {
+// - 16 - CMD/PRE/SGL/DAF/DRH/SGP - Seção de Gestão de Pessoas
+    if (model.cdOrgaoSetor === "16") {
+   model.arrayusu = ["SOFTPLAN", "JAILTON.PEREIRA"];
+ }  
+// 17 - CMD/PRE/SGL/DAF/DTI - Divisão de Tecnologia da Informação
+    if (model.cdOrgaoSetor === "17") {
+   model.arrayusu = ["JEFERSON", "EZEQUIEL"];
+ } 
+}, 2000);
