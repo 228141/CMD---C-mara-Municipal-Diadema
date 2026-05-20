8ad1d406de572fa20dcc6aa4907a2f463b91a3dc
228141 <jailton.sp@hotmail.com>
2026-05-20 10:16:29 -0300

    Atualizações de scripts

---
+++ BPM/concatena_descricao_abono_falta.js
@@ -0,0 +1,5 @@
+// Inserir o nome na descrição do formulário de abertura    
+
+model.descricao =
+    "Solicitação de abono de falta.\n" +
+    model.nomeSolicitante + ", ocupando a função de  Assistente Legislativo VI, não tendo comparecido ao serviço no dia" + model.solicita_dia + ", vem  respeitosamente, requerer a Vossa Senhoria ABONO DE FALTA, nos termos do Artigo 124, item I, parágrafo 1º, da Lei Complementar nº 08/91 -   Estatuto dos Funcionários Públicos de Diadema, tendo em vista o(s) seguinte(s) MOTIVO(S):  PARTICULARES";
